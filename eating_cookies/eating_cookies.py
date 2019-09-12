e525159a155a334bec768964cf230c025c1b6d8d
Matt Klein <mklein246@gmail.com>
2019-09-11 17:22:32 -0700

    used my crummy slow cookie function to get values. then saw the pattern in those values, and used it to make a much better cookie function. Cowabunga!

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -10,55 +10,32 @@ import sys
 
 # Cookie Monster can eat range(0, 4) cookies
 # It looks like with "0", they're just referring to the edge case of n = 0
-# def eating_cookies(n, cache=None):
-#     if n == 0:
-#         print("FINAL:", 1)
-#         return 1
-    
-#     if not cache or isinstance(cache, list):
-#         cache = {}
-#         cache["count"] = 0
-#         cache["max"] = n
-#         n = []
-#         cur_sum = 0
-#     else:
-#         cur_sum = sum(n)
 
-#     if cur_sum < cache["max"]:
-#         for i in (j for j in range(1, cache["max"] + 1 - cur_sum) if j <= 3):
-#             try:
-#                 return cache[f"{n + [i]}"]
-#             except:
-#                 cache[f"{n + [i]}"] = True
-#                 eating_cookies(n + [i], cache)
-#     else:
-#         cache["count"] += 1
-    
-#     return cache["count"]
+# 1, 1, 2, 4, 7, 13, 24, 44, 81, 149
+# seen that before
 
 def eating_cookies(n, cache=None):
     if not cache:
-        cache = [0 for i in range(n)]
+        cache = [0 for i in range(n + 1)]
 
-    if n == 0:
+    if cache[n] != 0:
+        return cache[n]
+
+    elif n < 2:
         return 1
 
-    if n == 1:
-        if cache[1] == 0:
-            cache[1] = 1
-        return 1
-
-    if n == 2:
-        if cache[2] == 0:
-            cache[2] = 2
+    elif n == 2:
         return 2
+    
+    else:
+        cache[n - 3] = eating_cookies(n - 3, cache)
+        cache[n - 2] = eating_cookies(n - 2, cache)
+        cache[n - 1] = eating_cookies(n - 1, cache)
+        return cache[n - 3] + cache[n - 2] + cache[n - 1]
+        
 
-    if n == 3:
-        if cache[3] == 0:
-            cache[3] = 4
-        return 4
-
-    return len(cache)
+    
+    
 
 
 if __name__ == "__main__":
