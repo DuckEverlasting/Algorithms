126c5cc037df15c0035b0bde2dfb37a41435d67b
Matt Klein <mklein246@gmail.com>
2019-09-12 23:06:28 -0700

    Improved the making_change solution, though it's still not perfect. Can't figure out caching for that one.

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python
 import sys
 
-def making_change(amount, denominations, cache=None):
-    if not cache:
-        cache = {}
-
+# This assumes denominations are sorted, with the smallest denomination coming first.
+# It works without sorting, but is more efficient this way. (Still could probably be much more efficient though.)
+def making_change(amount, denominations):
     if amount == 0 or len(denominations) == 0:
         return 1
-    
-    if cache[f"{amount}_{denominations}"]:
-        print("DING")
-        return cache[f"{amount}_{denominations}"]
 
     count = 0
-    coin = denominations[0]
-    new_denom = denominations [1:]
+    coin = denominations[-1]
+    new_denom = denominations [:-1]
 
-    for i in range(0, (amount // coin) + 1):
-        if amount - (coin * i) == 0:
+    if len(new_denom) == 0:
+        if amount % coin == 0:
             count += 1
-        elif len(new_denom) > 0:
-            count += making_change(amount - coin * i, new_denom, cache)
-            
-    cache[f"{amount}_{denominations}"] = count
-    return count
 
-print("[1, 5, 10, 25, 50]", making_change(61, [1, 5, 10, 25, 50]))
+    else:
+        for i in range(0, (amount // coin) + 1):
+            if amount - (coin * i) == 0:
+                count += 1
+            elif len(new_denom) > 0:
+                count += making_change(amount - coin * i, new_denom)
+
+    return count
 
 
 if __name__ == "__main__":
